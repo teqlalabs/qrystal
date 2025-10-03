e2e96286306c903f8e2a6d714bf78c5399b733b6
teqlalabs <teclacapital@gmail.com>
2025-10-03 02:13:55 -0300

    initial

---
+++ benches/vrf_performance.rs
@@ -0,0 +1,10 @@
+// benches/vrf_performance.rs
+
+use criterion::{criterion_group, criterion_main, Criterion};
+
+fn bench_placeholder(_c: &mut Criterion) {
+    // This will be replaced by the actual VRF performance test later.
+}
+
+criterion_group!(benches, bench_placeholder);
+criterion_main!(benches);
